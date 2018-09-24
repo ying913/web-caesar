6d3f3cab4a5f15e5763d9621fce8e3e869f3340f
Jennifer Lu <jennifer.k.lu@hotmail.com>
2018-09-24 18:40:22 -0500

    Finished web-caesar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from caesar import rotate_string
-import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,40 +9,42 @@ form= """
 <html>
     <head>
          <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
-        <form action= "/encrypted", method="POST" id="caesar">
+        <form action= "/", method='POST'>
             <label for="rotate-by">Rotate by:</label>
-            <input id="rotate-by" type="text" name="rot" />
-            <textarea name="text" form="caesar"></textarea> 
-            <input type="submit" />
+            <input id="rotate-by" type="text" name="rot" value="0"/>
+            <textarea name="text">{0}</textarea> 
+            <input type="submit"/>
+        </form>
     </body>
 </html>
 """
 
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    text = str(request.form['text'])
+    rot = int(request.form['rot'])
+    encrypted_message = str(rotate_string(text, rot))
+    return form.format(encrypted_message)
+
 @app.route("/")
 def index():
-    return form
-
-@app.route("/encrypted", methods=['POST'])
-def encrypt():
-    text = request.form['text']
-    rot = int(request.form['rot'])
-    encrypted_message = rotate_string(text, rot)
-    return '<h1>' + cgi.escape(encrypted_message) + '</h1>'
+    return form.format("")
 
 app.run() 
